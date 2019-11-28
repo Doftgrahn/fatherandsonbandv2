c5002629bfec6cbb45f416fa86786d22b32dea68
Simon grahn <simon.grahn@gmail.com>
2019-11-28 15:39:41 +0100

    fix fetch problem

--- src/FatherAndSonBand.js
+++ src/FatherAndSonBand.js
@@ -1,18 +1,18 @@
-import React, {lazy, Suspense, useState} from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 /* Styles */
-import "./FatherAndSonBand.scss";
+import './FatherAndSonBand.scss';
 
 /*Components*/
-const Header = lazy(() => import("./components/header/header"));
-const Routing = lazy(() => import("./general/routing"));
-const Footer = lazy(() => import("./components/footer/Footer"));
+const Header = lazy(() => import('./components/header/header'));
+const Routing = lazy(() => import('./general/routing'));
+const Footer = lazy(() => import('./components/footer/Footer'));
 
 const FatherAndSonBand = () => {
     const [store, setStore] = useState([
-        {id: 1, amount: null},
-        {id: 2, amount: null}
+        { id: 1, amount: null },
+        { id: 2, amount: null }
     ]);
 
     const deleteStore = (store, delItem) => {};
@@ -20,7 +20,7 @@ const FatherAndSonBand = () => {
     const currentStore = store => setStore(store);
 
     return (
-        <div className="App">
+        <div className='App'>
             <Router>
                 <Suspense fallback={<h1>Right with ya, hold on a sec!</h1>}>
                     <Header />
--- src/components/band/Band.jsx
+++ src/components/band/Band.jsx
@@ -1,44 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Fade from 'react-reveal/Fade';
 
-/*Band Info, in dataFolder*/
-import { bandInfo } from './data/memberText';
-const url = 'http://localhost:1337/bands';
+import bandInfo from './data/memberText';
 
 const Band = () => {
-    const [band, setBand] = useState(null);
-    //const [band] = useState(bandInfo);
+    const [band] = useState(bandInfo || []);
 
-    useEffect(() => {
-        fetch(url)
-            .then(respJSON => respJSON.json())
-            .then(resp => {
-                setBand(resp);
-            })
-            .catch(err => console.log(err));
-    }, []);
-
-    let renderBand;
-    if (band)
-        renderBand = band.map((member, i) => (
-            <div key={i} className='b-band__member'>
-                <Fade cascade duration={300}>
+    const renderBand = band.map((member, i) => (
+        <div key={i} className='b-band__member'>
+            <Fade cascade duration={300}>
+                <figure>
                     <img
-                        src={`http://localhost:1337/${member.bandImg.url}`}
-                        /*src={member.bandImg.url}*/
+                        src={member.picture}
                         className='b-band__member-img'
                         alt={`Bandmember: ${member.name}`}
                     />
-                    <div className='b-band__member-info'>
-                        <h3>{member.name}</h3>
-                        <p className='b-band__member-instrument'>
-                            {member.instrument}
-                        </p>
-                        <p>{member.info}</p>
-                    </div>
-                </Fade>
-            </div>
-        ));
+                </figure>
+                <div className='b-band__member-info'>
+                    <h3>{member.name}</h3>
+                    <p className='b-band__member-instrument'>
+                        {member.instrument}
+                    </p>
+                    <p>{member.info}</p>
+                </div>
+            </Fade>
+        </div>
+    ));
 
     return (
         <section className='b-band' id='members'>
